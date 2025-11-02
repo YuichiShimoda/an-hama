a5136b5abd6925f8f422ac8ccedd1e92a1d655c6
YuichiShimoda <p_ce.p_ce.w_ld@icloud.com>
2025-11-02 10:55:40 +0900

    デザ認証済みユーザーがログインページにアクセスしたときにダッシュボードへリダイレクト

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectUsersTo(function (\Illuminate\Http\Request $request) {
+            return '/admin';
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
