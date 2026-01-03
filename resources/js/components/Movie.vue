ad76f5445a96dbf47f9b1f32eaf63020879ef638
YuichiShimoda <p_ce.p_ce.w_ld@icloud.com>
2026-01-03 10:45:20 +0900

    add button in playing

--- resources/js/components/Movie.vue
+++ resources/js/components/Movie.vue
@@ -21,8 +21,15 @@
 			<img class="icon" :src="'./image/movie/completed.svg'" alt="リピート">
 		</div>
 		<video ref="videoPlayer" class="play-movie"></video>
+		<div v-if="!completed" class="bottom-box">
+			<button v-if="currentVideo !== 'season1-2'" class="next-btn is-play" @click="changeVideo" :data-src="'season1-2'">
+				<p><span>季</span><span>節</span><span>の</span><span>つ</span><span>け</span><span>か</span><span>け</span><span>パ</span><span>ス</span><span>タ</span></p>
+			</button>
+			<button v-if="currentVideo !== 'new-year'" class="next-btn is-play" @click="changeVideo" :data-src="'new-year'">
+				<p><span>新</span><span>年</span><span>の</span><span>ご</span><span>挨</span><span>拶</span></p>
+			</button>
+		</div>
 		<div v-if="completed" class="bottom-box">
-
 			<button v-if="currentVideo !== 'season1-2'" class="next-btn" @click="changeVideo" :data-src="'season1-2'">
 				<p><span>季</span><span>節</span><span>の</span><span>つ</span><span>け</span><span>か</span><span>け</span><span>パ</span><span>ス</span><span>タ</span></p>
 			</button>
