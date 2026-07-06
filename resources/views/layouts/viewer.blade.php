241241b83299d17170d7516e5b19466a3ce3680a
YuichiShimoda <p_ce.p_ce.w_ld@icloud.com>
2026-07-06 21:35:09 +0900

    Improving top page load speed5

--- resources/views/layouts/viewer.blade.php
+++ resources/views/layouts/viewer.blade.php
@@ -19,28 +19,28 @@
 		<style>
 			@font-face {
 				font-family: 'miniwa-lt-v3';
-				src: url("{{ url('fonts/MinIWA-Lt-V3-Regular.woff2', [], true) }}") format('woff2');
+				src: url("{{ secure_asset('fonts/MinIWA-Lt-V3-Regular.woff2') }}") format('woff2');
 				font-weight: 400;
 				font-style: normal;
 				font-display: swap;
 			}
 			@font-face {
 				font-family: 'miniwa-md-v3';
-				src: url("{{ url('fonts/MinIWA-Md-V3-Regular.woff2', [], true) }}") format('woff2');
+				src: url("{{ secure_asset('fonts/MinIWA-Md-V3-Regular.woff2') }}") format('woff2');
 				font-weight: 400;
 				font-style: normal;
 				font-display: swap;
 			}
 			@font-face {
 				font-family: 'miniwa-bd-v3';
-				src: url("{{ url('fonts/MinIWA-Bd-V3-Regular.woff2', [], true) }}") format('woff2');
+				src: url("{{ secure_asset('fonts/MinIWA-Bd-V3-Regular.woff2') }}") format('woff2');
 				font-weight: 400;
 				font-style: normal;
 				font-display: swap;
 			}
 			@font-face {
 				font-family: 'miniwa-hv-v3';
-				src: url("{{ url('fonts/MinIWA-Hv-V3-Regular.woff2', [], true) }}") format('woff2');
+				src: url("{{ secure_asset('fonts/MinIWA-Hv-V3-Regular.woff2') }}") format('woff2');
 				font-weight: 400;
 				font-style: normal;
 				font-display: swap;
